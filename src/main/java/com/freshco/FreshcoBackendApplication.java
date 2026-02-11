357697c61acc33afd7c12f93f893cf57414b4242
Gulshan <gulshangupta003@gmail.com>
2026-02-11 17:29:55 +0530

    initial commit

---
+++ src/main/java/com/freshco/FreshcoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.freshco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshcoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreshcoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/freshco/FreshcoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freshco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshcoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
